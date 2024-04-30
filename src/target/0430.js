6e56de9de659bc6e5c327cb5bb2a70b14310f394
yanghuning <185471581@qq.com>
2024-04-30 15:15:01 +0800

    '配置优化'

--- src/target/0430.js
+++ src/target/0430.js
@@ -1,3 +1,15 @@
-"use strict";
-
-console.log('这是一个装饰器 Decorator 2024');
+import './section/sec01.js';
+// import './section/sec02.js';
+// import './section/sec03.js';
+// import './section/sec04.js';
+// import './section/sec05.js';
+// import './section/sec06.js';
+// import './section/sec07.js';
+// import './section/sec08.js';
+// import './section/sec09.js';
+// import './section/sec10.js';
+// import './section/sec11.js';
+// import './section/sec12.js';
+// import './section/sec13.js';
+// import './section/sec14.js';
+// import './section/sec15.js';
